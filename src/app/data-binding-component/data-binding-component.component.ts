cdd16efd81193b17bfe19a7d9d11c4a523199e19
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-08-24 12:25:27 +0530

    Added Data Binding Component

---
+++ src/app/data-binding-component/data-binding-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-binding-component.component.html',
+  styleUrl: './data-binding-component.component.css'
+})
+export class DataBindingComponentComponent {
+
+}
