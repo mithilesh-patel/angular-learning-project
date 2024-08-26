712c326b26510cd2cbfada62630d2b7dfc205555
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-08-26 23:22:31 +0530

    Feat: Structural Directory component created

---
+++ src/app/components/directive/structural-dir/structural-dir.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-dir',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './structural-dir.component.html',
+  styleUrl: './structural-dir.component.css'
+})
+export class StructuralDirComponent {
+
+}
