e11dabdb7ce97802d5b70d4712843f11859a0179
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-08-26 23:22:09 +0530

    Feat: Structural Directory component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { DataBindingComponentComponent } from './data-binding-component/data-binding-component.component';
+import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DataBindingComponentComponent],
+  imports: [RouterOutlet, StructuralDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
