ef00e7dec718db0ffff82246ec312efd5e014cd1
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-03 09:40:39 +0530

    *ngFor for iterating within table

--- src/app/components/directive/structural-dir/structural-dir.component.ts
+++ src/app/components/directive/structural-dir/structural-dir.component.ts
@@ -22,6 +22,14 @@ export class StructuralDirComponent {
 
   cityArray : string []=['Mumbai', 'Bengaluru', 'Chennai', 'Delhi'];
 
+  studList: any[]=[
+    {id:1, name:"A", city: "Mumbai", isActive: true},
+    {id:2, name:"B", city: "Pune", isActive: false},
+    {id:3, name:"C", city: "Raipur", isActive: false},
+    {id:4, name:"D", city: "Indore", isActive: true},
+    {id:5, name:"E", city: "Banaras", isActive: false},
+    {id:6, name:"F", city: "Bangalore", isActive: false},
+  ]
 
   showDiv1() {
     this.isDiv1Visible = true;
