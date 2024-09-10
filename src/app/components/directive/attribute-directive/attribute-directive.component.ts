d62a753cddcfa17facf58c9dcad6febc550d3f61
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-10 23:12:08 +0530

    Added *ngFor for iterating over the array

--- src/app/components/directive/attribute-directive/attribute-directive.component.ts
+++ src/app/components/directive/attribute-directive/attribute-directive.component.ts
@@ -17,6 +17,15 @@ export class AttributeDirectiveComponent {
   isCheckboxChecked: boolean=false;
   selectBoxValue : string = '';
 
+  studentList: any[]=[
+    {id:1, name:"A", gender: "Male", city: "Mumbai", isActive: true},
+    {id:2, name:"B", gender: "Male", city: "Pune", isActive: false},
+    {id:3, name:"C", gender: "Female", city: "Raipur", isActive: false},
+    {id:4, name:"D", gender: "Male", city: "Indore", isActive: true},
+    {id:5, name:"E", gender: "Male", city: "Banaras", isActive: false},
+    {id:6, name:"F", gender: "Female", city: "Bangalore", isActive: false},
+  ]
+
   changeToDanger() {
     this.div1bgColor = 'bg-danger';
   }
