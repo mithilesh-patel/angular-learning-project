6745c13abe8242e8ff14688efc561d480163b7cd
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-12 23:57:23 +0530

    Created navbar for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
-import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StructuralDirComponent, AttributeDirectiveComponent],
+  imports: [RouterOutlet, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
