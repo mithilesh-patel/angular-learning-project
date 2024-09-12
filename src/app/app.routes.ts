2c1c46812f45e6a1cb4496e9b21fa896d6f5948c
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-12 22:23:09 +0530

    Created Route component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { DataBindingComponentComponent } from './components/data-binding-component/data-binding-component.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { StructuralDirComponent } from './components/directive/structural-dir/structural-dir.component';
+import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //This is a route object
+  {
+    path: 'add-emp',
+    component: AddEmployeeComponent,
+  },
+  {
+    path: 'data-binding',
+    component: DataBindingComponentComponent,
+  },
+  {
+    path:'employee-list',
+    component:EmployeeListComponent
+  },
+  {
+    path:'structural-dir',
+    component:StructuralDirComponent
+  },
+  {
+    path:'attribute-dir',
+    component:AttributeDirectiveComponent
+  }
+];
