23595e6ffe45e3caeacd4b6885a4c4cef9dd4907
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-14 23:09:27 +0530

    ifElse control flow setup

--- src/app/components/controlflow/ifelse/ifelse.component.ts
+++ src/app/components/controlflow/ifelse/ifelse.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ifelse',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ifelse.component.html',
   styleUrl: './ifelse.component.css'
 })
 export class IfelseComponent {
+
+  isDiv1Visible: boolean = false;
+  isDiv2Visible: boolean = true;
+
+  inputValue1: string = '';
+  inputValue2: string = '';
+
+  subject: string = '';
+
+  checkboxIsActive: boolean = false;
+
+
+  showDiv1(){
+    this.isDiv1Visible=true;
+  }
+  
+  hideDiv1(){
+    this.isDiv1Visible=false;
+    
+  }
+
+  toggleDiv2(){
+    this.isDiv2Visible=!this.isDiv2Visible;
+  }
 
 }
