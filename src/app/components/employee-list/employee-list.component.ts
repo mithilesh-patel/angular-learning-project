e60c68bb177404a93ce0b139c52556493d95834d
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-14 21:05:31 +0530

    Calling a component from a method using Router from method

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styles: ['.primary{color:red}']
 })
 export class EmployeeListComponent {
+  
+  constructor (private router: Router  ){
 
+  }
+  
+  goToAttributeComponent(){
+    this.router.navigateByUrl("attribute-dir")
+  }
 }
