daba22f015265955fccbf7fa1aefa46066fd4eac
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-15 18:48:42 +0530

    Using @for for iterating over array

--- src/app/components/controlflow/for/for.component.ts
+++ src/app/components/controlflow/for/for.component.ts
@@ -5,8 +5,19 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './for.component.html',
-  styleUrl: './for.component.css'
+  styleUrl: './for.component.css',
 })
 export class ForComponent {
 
+  cityList: string[]=['Delhi', 'Mumbai', 'Jaipur', 'Raipur'];
+  fruitList: string[]=['Apple', 'Mango', 'Guava', 'Pomegranate', 'Watermelon'];
+
+  studList: any[] = [
+    { id: 1, name: 'A', city: 'Mumbai', isActive: true },
+    { id: 2, name: 'B', city: 'Pune', isActive: false },
+    { id: 3, name: 'C', city: 'Raipur', isActive: false },
+    { id: 4, name: 'D', city: 'Indore', isActive: true },
+    { id: 5, name: 'E', city: 'Banaras', isActive: false },
+    { id: 6, name: 'F', city: 'Bangalore', isActive: false },
+  ];
 }
