1f02831f6bd48f6b7074479b2d8d46eb4de68227
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-15 22:46:26 +0530

    Added homepage component to routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,14 @@ import { AttributeDirectiveComponent } from './components/directive/attribute-di
 import { SwitchComponent } from './components/controlflow/switch/switch.component';
 import { IfelseComponent } from './components/controlflow/ifelse/ifelse.component';
 import { ForComponent } from './components/controlflow/for/for.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 export const routes: Routes = [
-    //This is a route object
+  //This is a route object 
+  {
+    path: '',
+    component: HomepageComponent,
+  },
   {
     path: 'add-emp',
     component: AddEmployeeComponent,
@@ -19,27 +24,27 @@ export const routes: Routes = [
     component: DataBindingComponentComponent,
   },
   {
-    path:'employee-list',
-    component:EmployeeListComponent
+    path: 'employee-list',
+    component: EmployeeListComponent,
   },
   {
-    path:'structural-dir',
-    component:StructuralDirComponent
+    path: 'structural-dir',
+    component: StructuralDirComponent,
   },
   {
-    path:'attribute-dir',
-    component:AttributeDirectiveComponent
+    path: 'attribute-dir',
+    component: AttributeDirectiveComponent,
   },
   {
-    path:'for-component',
-    component:ForComponent
+    path: 'for-component',
+    component: ForComponent,
   },
   {
-    path:'ifelse-component',
-    component:IfelseComponent
+    path: 'ifelse-component',
+    component: IfelseComponent,
   },
   {
-    path:'switch-component',
-    component:SwitchComponent
+    path: 'switch-component',
+    component: SwitchComponent,
   },
 ];
