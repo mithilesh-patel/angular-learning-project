637d91162eecd63b3fb53a897843013ec779d5ba
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-16 00:22:51 +0530

    Added pipes in pipe component

--- src/app/components/pipe/pipe.component.ts
+++ src/app/components/pipe/pipe.component.ts
@@ -1,14 +1,23 @@
-import { LowerCasePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [UpperCasePipe, LowerCasePipe],
+  imports: [UpperCasePipe, LowerCasePipe, CurrencyPipe, TitleCasePipe, DatePipe,JsonPipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
  fruit1: string="apple";
  fruit2: string="MANGO";
+ amount = 123.45;
+ company = 'acme corporation';
+ purchasedOn = '2024-07-08';
+ jsonObj={
+  name:'Mithilesh',
+  state:'Karnataka',
+  city:'Bangalore',
+  roll:'301602216028'
+ }
 }
