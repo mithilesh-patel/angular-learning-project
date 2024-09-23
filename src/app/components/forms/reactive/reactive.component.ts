d9f59a493ccdd2d4909d8ed351054f073243d69f
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-23 23:29:55 +0530

    Submit form using Reactive form

--- src/app/components/forms/reactive/reactive.component.ts
+++ src/app/components/forms/reactive/reactive.component.ts
@@ -1,12 +1,13 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule , JsonPipe],
   templateUrl: './reactive.component.html',
-  styleUrl: './reactive.component.css'
+  styleUrl: './reactive.component.css',
 })
 export class ReactiveComponent {
   studentForm: FormGroup = new FormGroup({
@@ -17,6 +18,13 @@ export class ReactiveComponent {
     state: new FormControl(),
     zipcode: new FormControl(),
     invalidCheck: new FormControl(),
-  
-  })
+  });
+
+  formValue: any;
+
+  onSubmit(){
+
+    this.formValue = this.studentForm.value;
+    console.log( this.studentForm.value);
+  }
 }
