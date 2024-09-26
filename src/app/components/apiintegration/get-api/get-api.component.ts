a122ea9410c9529c70e1bbafb02a3c70f29bc2ba
Mithilesh Kumar Patel <mithileshpatel250@gmail.com>
2024-09-26 21:23:59 +0530

    Created get api component

---
+++ src/app/components/apiintegration/get-api/get-api.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-api.component.html',
+  styleUrl: './get-api.component.css'
+})
+export class GetApiComponent {
+
+}
